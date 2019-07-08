76a533a5e6a985b5a47a015c4d256bf350ae8fbf
nasirali1 <nasir.ali08@gmail.com>
2019-07-08 13:32:31 -0500

    fixed bugs and mounted bucket folder

--- mysql/initdb.d/cerebralcortex_mysql.sql
+++ mysql/initdb.d/cerebralcortex_mysql.sql
@@ -1,20 +1,5 @@
--- phpMyAdmin SQL Dump
--- version 4.6.6deb5
--- https://www.phpmyadmin.net/
---
--- Host: localhost:3306
--- Generation Time: Mar 25, 2019 at 05:03 PM
--- Server version: 5.7.25-0ubuntu0.18.04.2
--- PHP Version: 7.0.33-5+ubuntu18.04.1+deb.sury.org+1
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `cerebralcortex`
@@ -48,7 +33,6 @@ CREATE TABLE `corrected_metadata` (
   `status` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- --------------------------------------------------------
 
 --
 -- Table structure for table `data_replay`
@@ -231,6 +215,3 @@ ALTER TABLE `stream`
 --
 ALTER TABLE `user`
   MODIFY `row_id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
